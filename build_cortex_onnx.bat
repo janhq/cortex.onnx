d2600e3c2bf325c8f39199566f3248297ff0d528
vansangpfiev <sang@jan.ai>
2024-08-06 13:59:18 +0700

    chore: bump genai to 62daf5e

--- build_cortex_onnx.bat
+++ build_cortex_onnx.bat
@@ -1,5 +1,5 @@
 cmake -S ./third-party -B ./build_deps/third-party
 cmake --build ./build_deps/third-party --config Release -j4
 
-cmake -S .\onnxruntime-genai\ -B .\onnxruntime-genai\build -DUSE_DML=ON -DUSE_CUDA=OFF -DORT_HOME="./build_deps/ort" -DENABLE_PYTHON=OFF -DENABLE_TESTS=OFF -DENABLE_MODEL_BENCHMARK=OFF
+cmake -S .\onnxruntime-genai\ -B .\onnxruntime-genai\build -DUSE_DML=ON -DUSE_CUDA=OFF -DUSE_ROCM=OFF -DORT_HOME="./build_deps/ort" -DENABLE_PYTHON=OFF -DENABLE_TESTS=OFF -DENABLE_MODEL_BENCHMARK=OFF
 cmake --build .\onnxruntime-genai\build --config Release -j4
